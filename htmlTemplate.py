69705f35310912d77ba1fcb6688f630e2d459566
Khadeeja Sabreen <138309013+Khadeeja-FarmwiseAI@users.noreply.github.com>
2023-10-05 12:17:24 +0530

    Add files via upload

---
+++ htmlTemplate.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 58px;
+  max-height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/512/4712/4712106.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/512/149/149995.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
